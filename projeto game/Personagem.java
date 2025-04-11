b5cdf2684ceb8793a437fd8cc228c7a97216ebfe
Rebecca Beccari Fagundes <164184762+sallybato@users.noreply.github.com>
2025-04-11 11:28:50 -0300

    Create Personagem.java

---
+++ projeto game/Personagem.java
@@ -0,0 +1,16 @@
+public class Personagem {
+
+    String nome;
+    String arma;
+    float vida;
+    float dano;
+
+
+    Personagem(String nome, String arma, float vida, float dano){
+        this.nome = nome;
+        this.arma = arma;
+        this.vida = vida;
+        this.dano = dano;
+
+    }
+}
