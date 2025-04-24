d7094e39f7257ff74a8eae67a9673381c443948c
Rebecca Beccari Fagundes <164184762+sallybato@users.noreply.github.com>
2025-04-24 11:35:47 -0300

    Update Personagem.java

--- projeto game/Personagem.java
+++ projeto game/Personagem.java
@@ -1,16 +1,46 @@
 public class Personagem {
 
-    String nome;
-    String arma;
-    float vida;
-    float dano;
+    private String nome;
+    private String arma;
+    private int vida;
+    private String funcao;
 
-
-    Personagem(String nome, String arma, float vida, float dano){
+    Personagem(String nome, String arma, int vida, float dano, String funcao){
         this.nome = nome;
         this.arma = arma;
         this.vida = vida;
-        this.dano = dano;
+        this.funcao = funcao;
+
 
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getArma() {
+        return arma;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+    public String getFuncao(){
+        return funcao;
+    }
+
+    public void atacar(){
+    }
+
+
+    void receberdano(float dano) {
+        this.vida -= dano;
+        if (this.vida < 0)this.vida = 0;
+            }
+
+    public int valordano(){
+        return 10;
+    }
+
 }
+
