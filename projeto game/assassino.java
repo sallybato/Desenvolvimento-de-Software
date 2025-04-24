7af1188ff2b2a192a71b27268fa4e4ca79a61f6d
Rebecca Beccari Fagundes <164184762+sallybato@users.noreply.github.com>
2025-04-24 11:36:47 -0300

    Update assassino.java

--- projeto game/assassino.java
+++ projeto game/assassino.java
@@ -1,2 +1,10 @@
 public class Assassino extends Personagem{
+
+    Assassino(String nome, String arma, int vida, float dano, String funcao){
+        super(nome, arma, vida, dano,funcao);
+    }
+    @Override
+    public void atacar(){
+        System.out.println("| Assassino [" + getNome() + "] ataca com arma: " + getArma());
+    }
 }
