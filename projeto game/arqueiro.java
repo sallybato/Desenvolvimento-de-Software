c9cc6b5cd2319c4528ed0b2ccc07690b165efee3
Rebecca Beccari Fagundes <164184762+sallybato@users.noreply.github.com>
2025-04-24 11:36:25 -0300

    Update arqueiro.java

--- projeto game/arqueiro.java
+++ projeto game/arqueiro.java
@@ -1,2 +1,11 @@
 public class Arqueiro extends Personagem{
+
+    Arqueiro(String nome, String arma, int vida, float dano, String funcao){
+        super(nome, arma, vida, dano,funcao);
+
+    }
+    @Override
+    public void atacar(){
+        System.out.println("|[Arqueiro] " + getNome() + " ataca com arma: " + getArma());
+    }
 }
