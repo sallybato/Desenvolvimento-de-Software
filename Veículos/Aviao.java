ef9a4befa6756c44d4fe232a06d6122c8c0cb032
Rebecca Beccari Fagundes <164184762+sallybato@users.noreply.github.com>
2025-04-25 11:13:17 -0300

    Create Aviao.java

---
+++ Veículos/Aviao.java
@@ -0,0 +1,24 @@
+public class Aviao extends VeiculoAereo {
+    private float envergaduraAsa;
+    private String tipoCombustivel;
+
+    public Aviao(int ano, String cor, String modelo, boolean Ligado, int numeroMotores,
+                 float envergaduraAsa, String tipoCombustivel) {
+
+        super(ano, cor, modelo, Ligado, numeroMotores);
+        this.envergaduraAsa = envergaduraAsa;
+        this.tipoCombustivel = tipoCombustivel;
+    }
+
+    void decolar() {
+        System.out.println("••••••••••••••••••••••••••••••••••••••••");
+        System.out.println("•O" + getModelo() + "acaba de decolar☻•");
+        System.out.println("••••••••••••••••••••••••••••••••••••••••");
+    }
+
+    void pousar() {
+        System.out.println("•••••••••••••••••••••••••••••••••••••••");
+        System.out.println("•O" + getModelo() + "acaba de pousar☻•");
+        System.out.println("•••••••••••••••••••••••••••••••••••••••");
+    }
+}
