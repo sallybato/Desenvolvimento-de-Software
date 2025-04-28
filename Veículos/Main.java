9e54a5f6882b66ec71f3cfad34c2e20b021895cd
Rebecca Beccari Fagundes <164184762+sallybato@users.noreply.github.com>
2025-04-28 10:58:36 -0300

    Update Main.java

--- Veículos/Main.java
+++ Veículos/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
+        Aviao a = new Aviao(2011, "branco","airbus",false,
+                2,60.93f,"diesel turbo caralhao");
+        a.ligar();
+        a.decolar();
+        a.pousar();
+        a.desligar();
+
+        Helicoptero h = new Helicoptero(2006,"pretao","airbus H125",true,
+                4,245.50f);
+
+            h.ligar();
+            h.pairar();
+            h.resgatarPessoas();
+            h.desligar();
+
     }
 }
