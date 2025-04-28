cb666bb81c08c2b2c6604e2db8fc10b8749160ed
Rebecca Beccari Fagundes <164184762+sallybato@users.noreply.github.com>
2025-04-28 11:00:02 -0300

    Update VeiculoAereo.java

--- Veículos/VeiculoAereo.java
+++ Veículos/VeiculoAereo.java
@@ -1,10 +1,10 @@
 public class VeiculoAereo extends Veiculo{
-   private int numeroMotores;
+    private int numeroMotores;
 
-   public VeiculoAereo(int ano, String cor, String modelo, boolean Ligado,int numeroMotores) {
-      super(ano, cor, modelo, Ligado);
-      this.numeroMotores = numeroMotores;
-   }
+    public VeiculoAereo(int ano, String cor, String modelo, boolean Ligado,int numeroMotores) {
+        super(ano, cor, modelo, Ligado);
+        this.numeroMotores = numeroMotores;
+    }
 
 
 }
